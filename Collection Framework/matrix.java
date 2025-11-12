8f9aa8eecc3785b7917311a42316b491d0aa1411
prateek <iamprateek22@gmail.com>
2025-11-13 00:30:05 +0530

    added code

---
+++ Collection Framework/matrix.java
@@ -0,0 +1,18 @@
+
+public class matrix {
+    public static void main(String [] args){
+        
+        int n = 3, m = 3;
+        // create 2d array
+        int [][] mat = new int[n][m];
+        
+        // initalize with 0
+        for(int i = 0; i<n ; i++){
+            for(int j = 0; j<m; j++){
+                mat[i][j] = 0;
+            }
+        }
+
+        
+    }    
+}
--- Collection Framework/usePriorityQueue.java
+++ Collection Framework/usePriorityQueue.java
@@ -1,3 +1,10 @@
+/*
+add() :  Inserts the specified element to the queue. If the queue is full, it throws an exception. Time Complexity: O(logN) where N denotes the number of elements present in the PriorityQueue.
+offer() - Inserts the specified element to the queue. If the queue is full, it returns false. Time Complexity: O(logN)
+poll() - returns and removes the head of the queue. Time Complexity: O(logN)
+peek() - return the head of the queue. Time Complexity: O(1)
+ */
+
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -7,6 +14,9 @@ public class usePriorityQueue {
         
         // pq is priority queue & comparator is used to define the priority to highest element first
         Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        
+        // using the PQ package, without argument so, smallest element first.
+        // PriorityQueue pq = new PriorityQueue<>();
 
         pq.offer(12);
         pq.offer(22);
